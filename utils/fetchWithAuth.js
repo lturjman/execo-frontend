dec3fbee6b11d78f4b6c3c9a516bf7f21de1b0f4
Laura Turjman <contact@lauraturjman.fr>
2025-07-17 17:20:04 +0200

    add utils fetchwithAuth and replace fetch

---
+++ utils/fetchWithAuth.js
@@ -0,0 +1,12 @@
+export const fetchWithAuth = async (url, options = {}) => {
+  const token = localStorage.getItem("token");
+
+  return fetch(url, {
+    ...options,
+    headers: {
+      ...(options.headers || {}),
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  });
+};
